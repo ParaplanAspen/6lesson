f4076a51f6ebd14a2c5f99969a7543da89b33376
ParaplanAspen <south_w@ukr.net>
2021-10-26 00:30:37 +0300

    Pizza

---
+++ src/com/company/Pizza.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Pizza {
+    Circle basic;
+    String composiyion;
+    String name;
+    double price;
+    double dianeter;
+    Pizza(Circle bas,String com,String nam,double pr, double diam){
+        basic=bas;
+        composiyion=com;
+        name=nam;
+        price=pr;
+        dianeter=diam;
+
+    }
+
+}
