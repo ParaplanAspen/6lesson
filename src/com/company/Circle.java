f0a2b3a01c45af3883f17d560a8d9ed9023ef10b
ParaplanAspen <south_w@ukr.net>
2021-10-26 00:20:03 +0300

    New homeWork

---
+++ src/com/company/Circle.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Circle {
+    double radius; //радиус окружности
+    double circumference; //длина окружности
+    double square; //площадь окружности
+    String colour; // цвет окружности
+    Circle (double rad) {
+        radius=rad;
+    }
+
+    Circle (double radi, String col) {
+        radius = radi;
+        colour = col;
+
+    }
+    double circumference(){
+        return 2*radius*Math.PI;
+    }
+    double square(){
+        return Math.PI*radius*radius;
+    }
+
+    public static void main(String[] args) {
+        Circle newObject=new Circle(12,"white");
+        double circumference;
+        double square;
+        String colour;
+        circumference=newObject.circumference();
+        square=newObject.square();
+        colour=newObject.colour;
+        System.out.println("Длина окружности равна"+" "+circumference );
+        System.out.println("Площадь окружности"+" "+square );
+        System.out.println("Цветокружности "+" "+colour );
+
+    }
+
+}
+
